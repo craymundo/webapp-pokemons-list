9d744f70af1d383a69d65b3603c23838ca37795d
Cesar Alvarado <desarrollador116_usi@minedu.gob.pe>
2021-04-06 18:09:26 -0500

    fix: corrección por ejecución de lint

--- src/libs/core/usecases/interactor/getAllPokemons.ts
+++ src/libs/core/usecases/interactor/getAllPokemons.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from "inversify";
 import PokemonRepository from "../repository/pokemonRepository";
+import { Pokemon }  from "../../domain/entity";
 
 export interface GetAllPokemons {
-  execute(): Promise<any>;
+  execute(): Promise<Pokemon[]>;
 }
 
 @injectable()
@@ -11,7 +12,7 @@ export class GetAllPokemonsImpl implements GetAllPokemons {
     @inject("PokemonRepository") public productRepo: PokemonRepository
   ) {}
 
-  execute(): Promise<any>{
+  execute(): Promise<Pokemon[]>{
     return this.productRepo.getAll();
   }
 }
--- src/libs/core/usecases/interactor/getPokemon.ts
+++ src/libs/core/usecases/interactor/getPokemon.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from "inversify";
 import PokemonRepository from "../repository/pokemonRepository";
+import { Pokemon }  from "../../domain/entity";
 
 export interface GetPokemon {
-  execute(name: string): Promise<any>;
+  execute(name: string): Promise<Pokemon>;
 }
 
 @injectable()
@@ -11,7 +12,7 @@ export class GetPokemonImpl implements GetPokemon {
     @inject("PokemonRepository") public productRepo: PokemonRepository
   ) {}
 
-  execute(name: string): Promise<any>{
+  execute(name: string): Promise<Pokemon>{
     return this.productRepo.getPokemon(name);
   }
 }
